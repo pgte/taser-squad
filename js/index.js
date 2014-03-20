c3b059b3f16aa37366f9b3ed164d9e204dd6c3bb
Pedro Teixeira <i@pgte.me>
2014-03-20 21:11:19 +0000

    experimenting with walls

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
 var tilemap = require('tilemap');
 
 var grid = tilemap(window.outerWidth, window.outerHeight);
-grid.appendTo(document.body);
+grid.on('createPoint', onCreatePoint);
+grid.zoom(0.5);
 
-for (var x = -10; x < 10; x++) {
-  for (var y = -10; y < 10; y++) {
+var size = 10;
+
+for (var x = -size / 2; x < size / 2; x++) {
+  for (var y = -size / 2; y < size / 2; y++) {
     createTile(x, y);
   }
 }
 
+grid.appendTo(document.body);
+
 window.addEventListener('resize', function (ev) {
-    grid.resize(window.outerWidth, window.outerHeight);
+  grid.resize(window.outerWidth, window.outerHeight);
+});
+
+var mode = 'tile';
+
+grid.on('keydown', function (ev) {
+    if (String.fromCharCode(ev.keyCode) === 'W') {
+        if (selectedPoint) {
+            var s = selectedPoint;
+            selectedPoint = null;
+            s.emit('mouseout');
+        }
+        grid.release(mode);
+        mode = { tile : 'point', point : 'tile' }[mode];
+    }
 });
 
 function createTile (x, y) {
@@ -37,5 +56,47 @@ function createTile (x, y) {
       'http://substack.net/projects/datacenter/rack_0.png',
       tile.x, tile.y
     );
-});
+  });
+}
+
+var selectedPoint = null;
+function onCreatePoint (pt) {
+    pt.on('mouseover', function () {
+        if (mode !== 'point') return;
+
+        if (!selectedPoint
+        || selectedPoint.x === pt.x
+        || selectedPoint.y === pt.y) {
+            pt.element.toFront();
+            pt.element.attr('fill', 'rgba(255,0,0,1)');
+        }
+    });
+
+    pt.on('mouseout', function () {
+        if (mode !== 'point') return;
+
+        if (pt !== selectedPoint) {
+            pt.element.attr('fill', 'transparent');
+        }
+    });
+
+    pt.on('mousedown', function () {
+        if (mode !== 'point') return;
+        if (!selectedPoint) {
+            pt.element.attr('fill', 'rgb(0,255,255,1)');
+            selectedPoint = pt;
+            return;
+        }
+
+        var walls = {
+            left : 'http://substack.net/projects/datacenter/wall_left.png',
+            right : 'http://substack.net/projects/datacenter/wall_right.png'
+        };
+        var src = selectedPoint.y === pt.y ? walls.right : walls.left;
+        grid.createWall(src, selectedPoint, pt);
+
+        selectedPoint.element.attr('fill', 'transparent');
+        pt.element.attr('fill', 'transparent');
+        selectedPoint = null;
+    });
 }
